5add112da3435a7996c1726c9f143c4d6042677c
Tushar Kumar <tusharkumar@hotmail.com>
2025-03-28 08:09:48 +0530

    completed task 3 and 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,12 +29,39 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Task 3
 public_users.get('/author/:author',function (req, res) {
   const getAuthor = req.params.author;
+  const bookKeys = Object.keys(books);
+  const matchingBooks = [];
+
+  bookKeys.forEach (key => {
+    if (books[key].author === getAuthor) {
+      matchingBooks.push(books[key]);
+    }
+  });
+  if (matchingBooks.length > 0) {
+    res.send(matchingBooks);
+  } else {
+    res.status(404).send("No books found by that author.");
+  }
 });
 
-// Get all books based on title
+// Task 4
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const getTitle = req.params.title;
+  const bookKeys = Object.keys(books);
+  const matchingBooks = [];
+
+  bookKeys.forEach (key => {
+    if (books[key].title === getTitle) {
+      matchingBooks.push(books[key]);
+    }
+  });
+  if (matchingBooks.length > 0) {
+    res.send(matchingBooks);
+  } else {
+    res.status(404).send("No books found by that author.");
+  }
+
 });
 
 //  Get book review
