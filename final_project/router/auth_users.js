b81e73a135257883db9679e64795a740073d0dab
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-01 07:39:28 +0530

    completed task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,17 +5,38 @@ const regd_users = express.Router();
 
 let users = [];
 
+// Task 7
+
 const isValid = (username)=>{
-  
+  return users.some((user) => user.username === username);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username,password)=>{
+  let validLogin = false;
+  users.forEach((user) => {
+    if(user.username === username) {
+      if (user.password === password) {
+      validLogin = true;
+    }
+  }})
+  return validLogin;
+};
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  
+  if (!username || !password) {
+    return res.status(404).json({ message: "Please enter valid username and password" });
+  }
+  
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({data: password}, "access", { expiresIn: 3600 });
+    req.session.authorization = { accessToken,username };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
