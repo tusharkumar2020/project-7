edaeed98c35771a2d2b0938372b725bcf8fa2f39
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-02 07:36:27 +0530

    completed task 8 and 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -31,8 +31,13 @@ regd_users.post("/login", (req,res) => {
   }
   
   if (authenticatedUser(username, password)) {
-    let accessToken = jwt.sign({data: password}, "fingerprint_customer", { expiresIn: 3600 });
-    req.session.authorization = { accessToken };
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+
+    req.session.authorization = {
+      accessToken, username
+    };
     return res.status(200).json({ message: "User successfully logged in" });
   } else {
     return res.status(208).json({ message: "Invalid Login. Check username and password" });
@@ -46,18 +51,41 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   const review = req.query.review;
   const username = req.session.authorization.username;
 
-  if (!isbn || !review) {
-    return res.status(400).json({ message: "ISBN and review are required." });
+  if (!username) {
+    return res.status(401).json({ message: "User not authenticated." });
   }
-  if (books.hasOwnProperty(isbn)) {
-    const book = books[isbn];
-    book[isbn].reviews[username] = review;
+
+  const book = books[isbn];
+
+  if (book) {
+    book.reviews[username] = review;
     return res.status(200).json({ message: "Review added successfully." });
   } else {
-      return res.status(404).json({ message: "Book not found." });
+    return res.status(404).json({ message: "Book not found." });
   }
+});
+
+// Task 9
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (!username) {
+    return res.status(401).json({ message: "User not authenticated." });
   }
-);
+
+  const book = books[isbn];
+
+  if (book) {
+    if (book.reviews[username]) {
+      delete book.reviews[username];
+      return res.status(200).json({ message: "Review deleted successfully." });
+    } else {
+      return res.status(404).json({ message: "Review not found." });
+    }
+  }
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
